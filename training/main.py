20580ec2317ea173d32146e8feb942b58a8a58f2
BoyuanJackChen <bc3194@nyu.edu>
2022-04-15 14:23:19 -0400

    refractor a function

--- training/main.py
+++ training/main.py
@@ -67,9 +67,9 @@ def main(args):
     for epoch in range(1, args.epochs + 1):
         print('Epoch:', epoch)
         train_epoch(model, optimizer, train_loader, train_loss_history)
-        evaluate(model, test_loader, test_loss_history)
+        evaluate_model(model, test_loader, test_loss_history)
     print('Execution time:', '{:5.2f}'.format(time.time() - start_time), 'seconds')
-    evaluate(model, test_loader, test_loss_history)
+    evaluate_model(model, test_loader, test_loss_history)
 
 
 
@@ -90,29 +90,6 @@ def train_epoch(model, optimizer, data_loader, loss_history):
                   '{:6.4f}'.format(loss.item()))
             loss_history.append(loss.item())
 
-
-def evaluate(model, data_loader, loss_history):
-    model.eval()
-
-    total_samples = len(data_loader.dataset)
-    correct_samples = 0
-    total_loss = 0
-
-    with torch.no_grad():
-        for data, target in data_loader:
-            output = F.log_softmax(model(data), dim=1)
-            loss = F.nll_loss(output, target, reduction='sum')
-            _, pred = torch.max(output, dim=1)
-
-            total_loss += loss.item()
-            correct_samples += pred.eq(target).sum()
-
-    avg_loss = total_loss / total_samples
-    loss_history.append(avg_loss)
-    print('\nAverage test loss: ' + '{:.4f}'.format(avg_loss) +
-          '  Accuracy:' + '{:5}'.format(correct_samples) + '/' +
-          '{:5}'.format(total_samples) + ' (' +
-          '{:4.2f}'.format(100.0 * correct_samples / total_samples) + '%)\n')
 
 
 
--- training/utils.py
+++ training/utils.py
@@ -111,6 +111,30 @@ def get_data_loader(args, train_kwargs, test_kwargs):
     return train_loader, test_loader
 
 
+def evaluate_model(model, data_loader, loss_history):
+    model.eval()
+
+    total_samples = len(data_loader.dataset)
+    correct_samples = 0
+    total_loss = 0
+
+    with torch.no_grad():
+        for data, target in data_loader:
+            output = F.log_softmax(model(data), dim=1)
+            loss = F.nll_loss(output, target, reduction='sum')
+            _, pred = torch.max(output, dim=1)
+
+            total_loss += loss.item()
+            correct_samples += pred.eq(target).sum()
+
+    avg_loss = total_loss / total_samples
+    loss_history.append(avg_loss)
+    print('\nAverage test loss: ' + '{:.4f}'.format(avg_loss) +
+          '  Accuracy:' + '{:5}'.format(correct_samples) + '/' +
+          '{:5}'.format(total_samples) + ' (' +
+          '{:4.2f}'.format(100.0 * correct_samples / total_samples) + '%)\n')
+
+
 if __name__=='__main__':
     path = "../checkpoints/Mixup/lr1e-3/e300_b64_lr0.001.pt"
     analyze_checkpoint(path)
